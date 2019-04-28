5c3a31570d34b4aa29537316879a5c7d2ddf5b04
marpot <marcin.potoczny@protonmail.com>
2019-04-28 23:28:44 +0200

    Chat in SpringBoot

---
+++ src/main/java/com/example/websocketdemo/WeboscketDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeboscketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeboscketDemoApplication.class, args);
+	}
+
+}
